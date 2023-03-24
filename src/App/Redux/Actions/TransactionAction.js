17e5e3cce727d2e477d24728219975f65a5b47ce
rvrobby1 <128361790+rvrobby1@users.noreply.github.com>
2023-03-24 15:13:37 +0530

    Delete src directory

--- src/App/Redux/Actions/TransactionAction.js
+++
@@ -1,8 +0,0 @@
-import * as actionType from "./actionsType";
-
-export const getTransactions = (payload) => {
-  return {
-    type: actionType.GET_TRANSACTIONS_LIST,
-    transactionPayload : payload
-  };
-};
--- src/App/Redux/rootSaga.js
+++
@@ -1,9 +0,0 @@
-import { all } from "redux-saga/effects";
-import AuthSaga from "./Sagas/AuthSaga";
-import WalletSaga from "./Sagas/WalletSaga";
-import DepositSaga from "./Sagas/DepositeSaga";
-import TransactionSaga from "./Sagas/TransactionsSaga";
-function* rootSaga() {
-  yield all([AuthSaga(), WalletSaga(), DepositSaga(),TransactionSaga()]);
-}
-export default rootSaga;
